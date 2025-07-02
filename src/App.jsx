e226b7729b65b34512c9e30a4a3a80e1b4183ca5
Avinash <avinashkumarofficial95@gmail.com>
2025-07-02 19:13:45 +0530

    batching setcounter more than once

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import './App.css'
 function App() {
  const [counter,setCounter] = useState(5);
   const handleAdd = () =>{
-    if(counter<=19) setCounter(counter+1);
+    setCounter((prev)=>prev+1);
+    setCounter((prev)=>prev+1);
+    setCounter((prev)=>prev+1);
   }
   const handleReduce = ()=>{
-    if(counter>=1) setCounter(counter-1);
+   setCounter(prev=>prev-1);
+   setCounter(prev=>prev-1);
+   setCounter(counter-1);
   }
   return (
     <>
