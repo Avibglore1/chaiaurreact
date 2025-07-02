21f9206d96b2b297d571222667ff36b3a6923a8f
Avinash <avinashkumarofficial95@gmail.com>
2025-07-02 21:34:00 +0530

    BACKGROUNDCHANGER

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import { useState } from 'react'
 import './App.css'
 function App() {
- const [counter,setCounter] = useState(5);
-  const handleAdd = () =>{
-    setCounter((prev)=>prev+1);
-    setCounter((prev)=>prev+1);
-    setCounter((prev)=>prev+1);
-  }
-  const handleReduce = ()=>{
-   setCounter(prev=>prev-1);
-   setCounter(prev=>prev-1);
-   setCounter(counter-1);
-  }
+ const [color,setColor] = useState('olive');
   return (
-    <>
-      <h1 className='font-bold bg- text-2xl'>Chai aur React</h1>
-      <h2 className='p-6 bg-yellow-400'>Counter: {counter}</h2>
-      <button onClick={handleAdd}>Increment</button>
-      <button onClick={handleReduce}>Decrement</button>
-    </>
+    <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+            <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 
+              rounded-3xl'>
+                <button
+                onClick={()=>setColor('red')}
+                className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+                style={{backgroundColor:'red'}}
+                >Red</button>
+                 <button
+                onClick={()=>setColor('green')}
+                className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+                style={{backgroundColor:'green'}}
+                >Green</button>
+                 <button
+                onClick={()=>setColor('blue')}
+                className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+                style={{backgroundColor:'blue'}}
+                >Blue</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
