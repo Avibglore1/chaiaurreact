e08cf0e02f6aa30b537672c7b8de58f1eaeee387
Avinash <avinashkumarofficial95@gmail.com>
2025-07-02 14:54:24 +0530

    counter example via react useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
 import './App.css'
-import Chai from './Chai'
 function App() {
-
+ const [counter,setCounter] = useState(5);
+  const handleAdd = () =>{
+    if(counter<=19) setCounter(counter+1);
+    
+  }
+  const handleReduce = ()=>{
+    if(counter>=1) setCounter(counter-1);
+    
+  }
   return (
     <>
       <h1>Chai aur React</h1>
-      <Chai/>
+      <h2>Counter: {counter}</h2>
+      <button onClick={handleAdd}>Increment</button>
+      <button onClick={handleReduce}>Decrement</button>
     </>
   )
 }
