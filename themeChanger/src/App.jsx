a6422698a7a66ec1069555abe625b4216ff91d3a
Avinash <avinashkumarofficial95@gmail.com>
2025-07-05 21:37:50 +0530

    theme toggler implemented

--- themeChanger/src/App.jsx
+++ themeChanger/src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 import './App.css'
+import { ThemeProvider } from './context/theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState('light')
 
+  const lightTheme = () =>{
+    setThemeMode('light');
+  }
+
+  const darkTheme = () =>{
+    setThemeMode('dark')
+  }
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove('light','dark');
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
   return (
-    <>
-        <h1 className='text-4xl text-gray-500 bg-pink-400'>Heading</h1>
-    </>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}} >
+        <div className='flex flex-wrap min-h-screen items-center'>
+        <div className='w-full'>
+            <div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
+                <ThemeBtn/>
+            </div>
+            <div className='w-full max-w-sm mx-auto'>
+                <Card/>
+            </div>
+        </div>
+    </div>
+    </ThemeProvider>
+    
   )
 }
 
